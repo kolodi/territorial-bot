6a29b28303aabc6039afd2c3b2ddfa747646d571
Michele Kolodi <mykolodiy@gmail.com>
2022-02-13 21:57:37 +0100

    basic interactions with commands

--- index.js
+++ index.js
@@ -1,33 +1,72 @@
-const { Client, Intents  } = require("discord.js");
+const { Client, Intents, MessageActionRow, MessageButton, MessageEmbed } = require("discord.js");
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.on("ready", () => console.log(`Logged in as ${client.user.tag}.`));
 
 client.on("interactionCreate", async (interaction) => {
-    if (!interaction.isCommand()) return;
+    if (interaction.isCommand()) {
+        const { commandName } = interaction;
 
-    const { commandName } = interaction;
+        console.log("Command received: ", commandName);
+        console.log("User: ", interaction.user.username);
 
-    switch (commandName) {
-        case "help":
-            interaction.reply("TBD: List of commands");
-            break;
-        case "ping":
-            interaction.reply("Pong!");
-            break;
-        case "add-coins":
-            interaction.reply("TBD: Add coins to a user.");
-            break;
-        default:
-            interaction.reply("Unknown command.");
-            break;
+        switch (commandName) {
+            case "help":
+                await interaction.reply({ content: "TBD: List of commands", ephemeral: true });
+                break;
+            case "ping":
+                await interaction.reply("Pong!");
+                break;
+            case "add-coins":
+                const options = interaction.options;
+                const userOpt = options.getMentionable("user");
+                if (!userOpt.user) {
+                    await interaction.reply({
+                        content: "You must mention a user.",
+                        ephemeral: true,
+                    });
+                    return;
+                }
+                const amount = options.getInteger("amount");
+                const message = `You want to add ${amount} coins to <@${userOpt.id}>?`;
+                const row = new MessageActionRow().addComponents(
+                    new MessageButton()
+                        .setCustomId("confirm_add")
+                        .setLabel("Confirm")
+                        .setStyle("PRIMARY"),
+                    new MessageButton()
+                        .setCustomId("cancel")
+                        .setLabel("Cancel")
+                        .setStyle("SECONDARY")
+                );
+                const embed = new MessageEmbed()
+                    .setColor("#0099ff")
+                    .setTitle("Add Coins")
+                    .setURL("https://discord.js.org")
+                    .setDescription(message);
+                console.log(message);
+                await interaction.reply({
+                    embeds: [embed],
+                    components: [row],
+                    ephemeral: true,
+                });
+                break;
+            default:
+                await interaction.reply("Unknown command.");
+                break;
+        }
+    } else if (interaction.isButton()) {
+      await interaction.reply({content: "Button pressed", ephemeral: true});
+    } else {
+      await interaction.reply({content: "Unknown interaction.", ephemeral: true});
     }
 });
 
-// bot.on('message', async message => {
-//   return;
-// });
+client.on("message", async (message) => {
+    console.log(message.content);
+    //message.reply("Hello!");
+});
 
 require("./server")();
 client.login(process.env.TOKEN);
--- register.js
+++ register.js
@@ -1,6 +1,10 @@
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
-const { SlashCommandBuilder } = require("@discordjs/builders");
+const {
+    SlashCommandBuilder,
+    SlashCommandSubcommandBuilder,
+    SlashCommandMentionableOption,
+} = require("@discordjs/builders");
 const commands = [
     new SlashCommandBuilder()
         .setName("help")
@@ -8,7 +12,29 @@ const commands = [
     new SlashCommandBuilder()
         .setName("ping")
         .setDescription("Checks connectivity with discord's servers."),
-    new SlashCommandBuilder().setName("add-coins").setDescription("Add coins to a user."),
+    new SlashCommandBuilder()
+        .setName("add-coins")
+        .setDescription("Add coins to a user.")
+        .addMentionableOption((opt) =>
+            opt.setName("user").setDescription("The user to add coins to.").setRequired(true)
+        )
+        .addIntegerOption((opt) =>
+            opt.setName("amount").setDescription("The amount of coins to add.").setRequired(true).setMinValue(10).setMaxValue(1000)
+        ),
+    // new SlashCommandBuilder().setName("add").addSubcommand(
+    //     new SlashCommandSubcommandBuilder()
+    //         .setName("coins")
+    //         .setDescription("Add coins to a user.")
+    //         .addMentionableOption(
+    //             new SlashCommandMentionableOption()
+    //               .setName("user")
+    //               .setDescription("The user to add coins to.")
+    //               .setRequired(true)
+    //         )
+    //         .addIntegerOption((opt) =>
+    //             opt.setName("amount").setDescription("The amount of coins to add.")
+    //         )
+    // ),
 ].map((command) => command.toJSON());
 
 const commandsBody = JSON.stringify(commands);
