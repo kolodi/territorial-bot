a56c5e2a36184a234ef068bcb6662a824401685f
StuffyNose69 <89052441+StuffyNose69@users.noreply.github.com>
2022-02-19 20:17:23 +0000

    Trying to add buttons to the info command.

--- commands/info.js
+++ commands/info.js
@@ -11,7 +11,21 @@ const execute = async (interaction, db, cache, client, totalCommandCounter) => {
   const minutes = Math.floor(client.uptime / 60000) % 60;
   const seconds = Math.floor(client.uptime / 1000) % 60;
   console.log(JSON.stringify(client.application, null, 2));
-  const info = new MessageEmbed()
+    const btnRow = new MessageActionRow().addComponents(
+      new MessageButton()
+        .setLabel('Normal Info')
+        .setStyle("PRIMARY")
+        .setCustomId("info"),
+      new MessageButton()
+        .setLabel('Bot Uptime')
+        .setStyle("PRIMARY")
+        .setCustomId("uptime"),
+      new MessageButton()
+        .setLabel('Bot Ping')
+        .setStyle("PRIMARY")
+        .setCustomId("ping"),
+    )
+    const ie = new MessageEmbed()
       .setColor(theme.mainColor)
       .setTitle("Botinfo Command")
       .setDescription(`TerraBot's Information`)
@@ -20,28 +34,70 @@ const execute = async (interaction, db, cache, client, totalCommandCounter) => {
       .addField("Created At", `<t:${getTimeInSeconds(client.application.createdTimestamp)}:D>`, true)
       .addField("Language", "**JavaScript** (node v16)", true)
       .addField("Library", "**discord.js** v13.6.0", true)
-      .addField("Current Latency", `**${Date.now() - interaction.createdTimestamp}**ms`, true)
-      .addField("Uptime", `**${days}**d **${hours}**h **${minutes}**m **${seconds}**s`, true)
       .setFooter(`v0.1 Alpha. Stage: ${process.env.STAGE}`)
       .setTimestamp();
-  interaction.reply({ embeds: [info], ephemeral: config.ephemeral });
+    const pe = new MessageEmbed()
+      .setTitle("Info Command")
+      .addField(":desktop: Current Latency", `**${Date.now() - interaction.createdTimestamp}**ms`, true)
+      .setFooter(`v0.1 Alpha. Stage: ${process.env.STAGE}`)
+      .setTimestamp();
+      .setColor(theme.mainColor)
+    const ue = new MessageEmbed()
+      .setTitle("Info Command")
+      .addField(":timer: Uptime", `**${days}**d **${hours}**h **${minutes}**m **${seconds}**s`, true)
+      .addField(":notepad_spiral: Commands Used", `The total amount of commands used since last restart is **${totalCommandCounter}**`)
+      .setFooter(`v0.1 Alpha. Stage: ${process.env.STAGE}`)
+      .setTimestamp();
+      .setColor(theme.mainColor)
+  interaction.reply({ content: 'This command gives you different information about the bot.', embeds: [ie], components: [btnRow] }).then(msg => {
+      const collecter = msg.createMessageComponentCollector({ time: 15000 })
+      collecter.on('collect', async i => {
+        if (i.customId === 'info') {
+          i.update({ content: 'Showing **Normal Info**', embeds: [ie], components: [btnRow] })
+        }
+        if (i.customId === 'uptime') {
+          i.update({ content: 'You have pressed another button!', embeds: [ue], components: [btnRow] })
+        }
+        if (i.customId === 'ping') {
+          i.update({ content: 'You have pressed a button!', embeds: [pe], components: [btnRow] })
+        }
+      })
+      collecter.on('collect', async collected => {
+        return;
+      })
+    })
 }
 
 module.exports.execute = execute;
 
 /**
- * const btnRowM1 = new MessageActionRow().addComponents(
+ * const btnRow = new MessageActionRow().addComponents(
       new MessageButton()
-        .setLabel('Menu 1 Embed 1')
+        .setLabel('Normal Info')
         .setStyle("PRIMARY")
         .setCustomId("info"),
       new MessageButton()
-        .setLabel('Menu 1 Embed 2')
+        .setLabel('Bot Uptime')
         .setStyle("PRIMARY")
         .setCustomId("uptime"),
       new MessageButton()
-        .setLabel('Menu 1 Embed 3')
+        .setLabel('Bot Ping')
         .setStyle("PRIMARY")
         .setCustomId("ping"),
     )
+ */
+
+/**
+ * interaction.reply({ content: 'test', embeds: [info], components: [btnRow] }).then(msg => {
+      const collecter = msg.createMessageComponentCollector({ time: 15000 })
+      collecter.on('collect', async i => {
+        if (i.customId === 'info') {
+          i.update({ content: 'You have pressed a button!', embeds: [m1], components: [btnRowM1] })
+        }
+        if (i.customId === 'uptime') {
+          i.update({ content: 'You have pressed another button!', embeds: [m2], components: [btnRowM2] })
+        }
+        if (i.customId === 'ping') {
+          i.update({ content: 'You have pressed a button!', embeds: [m1e1], components: [] })
+        }
  */
