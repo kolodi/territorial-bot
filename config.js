6668aac1d72647d1a516356982067604a324ce15
Michele Kolodi <mykolodiy@gmail.com>
2022-02-21 13:34:20 +0100

    remove firebase json

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const config = {
 	//get all files in "./commands" and remove the last 3 characters of them (usually ".js")
     commands: readdirSync('./commands').map(fn => fn.slice(0, -3)),
     admin_commands: ["coins", "ping", "logs", "whois", "uptime", "reload", "purge"], // reload is special as it is not in the list of all commands
-    ephemeral: false,
+    ephemeral: process.env.STAGE !== "DEV",
 };
 
 module.exports = config;
